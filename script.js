47969f4e8a100b3c4c09825e6e15a7e45576c524
Tri Wibowo Cahyo <twibowo288@gmail.com>
2026-03-26 21:22:30 +0700

    add new CTF (Charon) & change keyboard warrior writeup from docx to pdf

--- script.js
+++ script.js
@@ -74,10 +74,11 @@ const ctfData = [
     { title: "Credsnoop", type: "blue", desc: "PCAP & Event Log Correlation", url: "https://nickel-bedbug-8cc.notion.site/Credsnoop-Hacktrace-Ranges-300416e03a1380d6ac3dde6e3a934858" },
     { title: "Loki", type: "blue", desc: ".NET Reverse Engineering", url: "https://nickel-bedbug-8cc.notion.site/Loki-Hacktrace-Ranges-2fc416e03a1380fdb604e3e4c6553b22"},
     { title: "Cilent", type: "blue", desc:"Phishing HTML Attachment & Telegram Exfil", url: "https://nickel-bedbug-8cc.notion.site/CTF-Writeup-Cilent-Hacktrace-Defensive-321416e03a13803fa136e019d0515260"},
-    { title: "Keyboard Warrior", type: "blue", desc:"Malware Analysis", url: "https://docs.google.com/document/d/1n9O_sKtLHmfe_T4wEok8o_9h3orpX6zA/edit?usp=sharing&ouid=112970802675533255155&rtpof=true&sd=true"},
+    { title: "Keyboard Warrior", type: "blue", desc:"Malware Analysis", url: "https://drive.google.com/file/d/1PGZPKUSsNCi0HlMq5r4Hf6YLBEITMMve/view?usp=sharing"},
     { title: "Shortway", type: "blue", desc: "Malware Infection Analysis", url: "https://docs.google.com/document/d/1NV_NmB1Zby9emp42q3OGM4oxgGQbWb_S/edit?usp=sharing&ouid=112970802675533255155&rtpof=true&sd=true"},
     { title: "Volatify", type: "blue", desc: "Memory Forensics", url: "https://docs.google.com/document/d/1a0vIHmZ3PQi-7ZOk16lcNCtavdavIwR-/edit?usp=sharing&ouid=112970802675533255155&rtpof=true&sd=true"},
-    { title: "Subvert", type: "blue", desc: ".NET Malware Analysis & MSSQL Injection", url: "https://drive.google.com/file/d/1d60zUDWBP3id0whogPcUE-_QtQ7vF1FM/view?usp=sharing"}
+    { title: "Subvert", type: "blue", desc: ".NET Malware Analysis & MSSQL Injection", url: "https://drive.google.com/file/d/1d60zUDWBP3id0whogPcUE-_QtQ7vF1FM/view?usp=sharing"},
+    { title: "Charon", type: "blue", desc: "Memory & Disk Forensics", url: "https://drive.google.com/file/d/1om6jsQ3yypLkiA9K8eSZYzWfOcJT-yqe/view?usp=sharing"},
 ];
 
 // ── INIT ──────────────────────────────────────────────────────────────
